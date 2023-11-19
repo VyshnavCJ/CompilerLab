cf826e693974caabfa882f32526955d793e663c2
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2023-11-20 05:23:03 +0530

    Update exp9.c

--- exp9.c
+++ exp9.c
@@ -1,74 +1,71 @@
 #include<stdio.h>
-
-#include<math.h>
-
 #include<string.h>
-
 #include<ctype.h>
 
-#include<stdlib.h>
+int n;
+char a[10][10];
+void first(char ch);
+void follow(char ch);
 
-int n, m = 0, p, i = 0, j = 0;
-char a[10][10], f[10];
-void follow(char c);
-void first(char c);
-int main() {
+int main(){
+    int x;
+    char ch;
+    printf("Enter the no of prodction: ");
+    scanf("%d",&n);
+    
+    printf("Enter the productions: ");
+    for(int i=0;i<n;i++){
+        scanf("%s",a[i]);
+    }
+    getchar();
+    do{
+        printf("Enter the element to find first and follow: ");
+        scanf("%c",&ch);
+        printf("First(%c)= {",ch);
+        first(ch);
+        printf("}");
+        printf("\nFollow(%c)= {",ch);
+        follow(ch);
+        printf("}");
+        printf("\nDo you want to continue?(0/1)");
+        scanf("%d",&x);
+        getchar();
 
-  int i, z;
-  char c, ch;
-  //clrscr();
-  printf("Enter the no of prooductions:\n");
-  scanf("%d", & n);
-  printf("Enter the productions:\n");
-  for (i = 0; i < n; i++)
-    scanf("%s%c", a[i], & ch);
-  do {
-    m = 0;
-    printf("Enter the elemets whose fisrt & follow is to be found:");
-    scanf("%c", & c);
-    first(c);
-    printf("First(%c)={", c);
-    for (i = 0; i < m; i++)
-      printf("%c", f[i]);
-    printf("}\n");
-    strcpy(f, " ");
-    //flushall();
-    m = 0;
-    follow(c);
-    printf("Follow(%c)={", c);
-    for (i = 0; i < m; i++)
-      printf("%c", f[i]);
-    printf("}\n");
-    printf("Continue(0/1)?");
-    scanf("%d%c", & z, & ch);
-  } while (z == 1);
-  return (0);
+    }while(x==1);
+    return 0;
 }
-void first(char c) {
-  int k;
-  if (!isupper(c))
-    f[m++] = c;
-  for (k = 0; k < n; k++) {
-    if (a[k][0] == c) {
-      if (a[k][2] == '$')
-        follow(a[k][0]);
-      else if (islower(a[k][2]))
-        f[m++] = a[k][2];
-      else first(a[k][2]);
+
+void first(char ch){
+    if(islower(ch)){
+        printf("%c, ",ch);
+        return;
+    }else{
+        for(int i=0;i<n;i++){
+            if(a[i][0]==ch){
+                if(islower(a[i][2])){
+                    printf("%c, ",a[i][2]);
+                }else{
+                    first(a[i][2]);
+                }
+            }
+        }
     }
-  }
 }
-void follow(char c) {
-  if (a[0][0] == c)
-    f[m++] = '$';
-  for (i = 0; i < n; i++) {
-    for (j = 2; j < strlen(a[i]); j++) {
-      if (a[i][j] == c) {
-        if (a[i][j + 1] != '\0')
-          first(a[i][j + 1]);
-        if (a[i][j + 1] == '\0' && c != a[i][0])
-          follow(a[i][0]);
-      }
+
+void follow(char ch){
+    if(a[0][0]==ch){
+        printf("$");
     }
-  }
+    for(int i=0;i<n;i++){
+        for(int j=2;j<strlen(a[i]);j++){
+            if(a[i][j]==ch){
+                if(a[i][j+1]!='\0'){
+                    first(a[i][j+1]);
+                }else if(a[i][0]!=ch){
+                    follow(a[i][0]);
+                }
+                
+            }
+        }
+    }
 }
