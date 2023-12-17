c88fb5059459734360b98195685fdf9f94b49d23
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2023-12-17 08:10:26 +0530

    Update exp11.c

--- exp11.c
+++ exp11.c
@@ -1,77 +1,103 @@
 // Construct a Shift Reduce Parser for a given language.
 
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-#include<stdlib.h>
+char a[10], stk[10];
+int i = 0, j = 0, z = 0, c;
 
-#include<string.h>
-
-int z = 0, i = 0, j = 0, c = 0;
-char a[16], ac[20], stk[15], act[10];
-
-void check() {
-  strcpy(ac, "REDUCE TO E -> ");
-
-  for (z = 0; z < c; z++) {
-    if (stk[z] == '4') {
-      printf("%s4", ac);
-      stk[z] = 'E';
-      stk[z + 1] = '\0';
-
-      printf("\n$%s\t%s$\t", stk, a);
+void check()
+{
+    for (z = 0; z < c - 1; z++)
+    {
+        if (stk[z] == 'i' && stk[z + 1] == 'd')
+        {
+            printf("Reduce to E=id\n");
+            stk[z] = 'E';
+            stk[z + 1] = '\0';
+            j++;
+            printf("\n$%s\t%s$", stk, a);
+        }
     }
-  }
-
-  for (z = 0; z < c - 2; z++) {
-    if (stk[z] == '2' && stk[z + 1] == 'E' && stk[z + 2] == '2') {
-      printf("%s2E2", ac);
-      stk[z] = 'E';
-      stk[z + 1] = '\0';
-      stk[z + 2] = '\0';
-      printf("\n$%s\t%s$\t", stk, a);
-      i = i - 2;
+    for (z = 0; z < c - 2; z++)
+    {
+        if (stk[z] =='E' && stk[z + 1] == '+' && stk[z + 2] == 'E')
+        {
+            printf("Reduce to E=E+E\n");
+            stk[z] = 'E';
+            stk[z + 1] = '\0';
+            stk[z + 2] = '\0';
+            i = i - 2;
+            printf("\n$%s\t%s$", stk, a);
+        }
     }
-  }
-
-  for (z = 0; z < c - 2; z++) {
-    if (stk[z] == '3' && stk[z + 1] == 'E' && stk[z + 2] == '3') {
-      printf("%s3E3", ac);
-      stk[z] = 'E';
-      stk[z + 1] = '\0';
-      stk[z + 1] = '\0';
-      printf("\n$%s\t%s$\t", stk, a);
-      i = i - 2;
+    for (z = 0; z < c - 2; z++)
+    {
+        if (stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E')
+        {
+            printf("Reduce to E=E*E\n");
+            stk[z] = 'E';
+            stk[z + 1] = '\0';
+            stk[z + 2] = '\0';
+            i = i - 2;
+            printf("\n$%s\t%s$", stk, a);
+        }
     }
-  }
-  return;
+    for (z = 0; z < c - 2; z++)
+    {
+        if (stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')')
+        {
+            printf("Reduce to E=(E)\n");
+            stk[z] = 'E';
+            stk[z + 1] = '\0';
+            stk[z + 2] = '\0';
+            i = i - 2;
+            printf("\n$%s\t%s$", stk, a);
+        }
+    }
 }
 
-int main() {
-  printf("GRAMMAR is -\nE->2E2 \nE->3E3 \nE->4\n");
+int main()
+{
+    printf("Grammar is \nE=E+E\nE=E*E\nE=(E)\nE=id");
+    printf("\nEnter the input string: ");
+    scanf("%s", a);
+    c = strlen(a);
+    printf("\nStack\tInput\tAction");
+    printf("\n$\t%s$\t", a);
+    for (i = 0; j < c; i++, j++)
+    {
 
-  printf("Enter input string: ");
-  scanf("%s", a);
-  c = strlen(a);
-  strcpy(act, "SHIFT");
+        printf("shift");
 
-  printf("\nstack \t input \t action");
-  printf("\n$\t%s$\t", a);
-  for (i = 0; j < c; i++, j++) {
-    printf("%s", act);
-
-    stk[i] = a[j];
-    stk[i + 1] = '\0';
-    a[j] = ' ';
-
-    printf("\n$%s\t%s$\t", stk, a);
-
+        if (a[j] == 'i' && a[j + 1] == 'd')
+        {
+            stk[i] = a[j];
+            stk[i + 1] = a[j + 1];
+            stk[i + 2] = '\0';
+            a[j] = ' ';
+            a[j + 1] = ' ';
+        }
+        else
+        {
+            stk[i] = a[j];
+            stk[i + 1] = '\0';
+            a[j] = ' ';
+        }
+        printf("\n$%s\t%s$\t", stk, a);
+        check();
+    }
     check();
-  }
 
-  check();
-
-  if (stk[0] == 'E' && stk[1] == '\0')
-    printf("Accept\n");
-  else
-    printf("Reject\n");
+    if (stk[0] == 'E' && stk[1] == '\0')
+    {
+        printf("\nInput Accepted!");
+    }
+    else
+    {
+        printf("\nInput Rejected!");
+    }
+    return 0;
 }
